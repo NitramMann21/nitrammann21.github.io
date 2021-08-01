7ac5026fa98c9de9b91d404353a097c6fe93074f
NitramMann21 <martin@arnomann.de>
2021-08-01 18:25:19 +0200

    Update JTA JavaDoc to 2.1.0

--- jta/javadocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"de.arnomann.martin.jta.annotations"},{"l":"de.arnomann.martin.jta.api"},{"l":"de.arnomann.martin.jta.api.entities"},{"l":"de.arnomann.martin.jta.api.events"},{"l":"de.arnomann.martin.jta.api.exceptions"},{"l":"de.arnomann.martin.jta.api.requests"},{"l":"de.arnomann.martin.jta.api.util"}]
